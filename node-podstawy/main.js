6f97f65cd8a0b69f09b391b697626184dfc82c36
Sylwester Kobel <sylwestermk94@gmail.com>
2022-10-09 11:28:51 +0200

    v3

--- node-podstawy/main.js
+++ node-podstawy/main.js
@@ -80,7 +80,7 @@ const names = ['Damian', 'Paweł', 'Dominik'];
 
 const shopping = [
   {
-    name: "banan",
+    name: "Banan",
     price: 3.99
   },
   {
@@ -151,10 +151,6 @@ console.log(sum);
 
 
 
-
-
-
-
 // sort
 // reduce
 
@@ -162,3 +158,265 @@ console.log(sum);
 // Properties
 
 // .length dlugosc
+
+
+// Cwiczenia forEach, map, filter
+
+// Mamy listę ksiązek
+
+
+
+// const books = [
+//   {
+//     name: "Harry Potter",
+//     category: "adventure"
+//   },
+//   {
+//     name: 'Alice in Wonderland',
+//     category: 'fantasy'
+//   },
+//   {
+//     name: 'Lord of the Rings',
+//     category: 'fantasy'
+//   }
+// ]
+
+// 1. Przy uzyciu funkcji filter, wyswietl tablice ksiazek z kategorii adventure
+
+// const adventureBooks = books.filter(book => {
+//   return book.category === 'adventure'
+// })
+
+// console.log(adventureBooks)
+
+// 2. Przy uzyciu funkcji filter, wyszukaj elementy, ktore maja w swojej nazwie ciag znakow 'er'
+
+// const booksWithPhraseEr = books.filter(book => {
+//   return book.name.includes('er');
+// })
+
+// console.log(booksWithPhraseEr)
+
+// 3. Przy uzyciu funkcji map, stworz tablice zawierajaca ile jest liter w nazwie ksiazki
+
+// const letterFromBooks = books.map(book => {
+//   return book.name.length
+// });
+
+// console.log(letterFromBooks)
+
+
+
+
+
+
+// !---------------- Funkcje ------------------------
+
+function sumDigits(a, b) {
+  return a + b;
+}
+
+console.log(sumDigits(2, 3));
+console.log(sumDigits(5, 7));
+console.log(sumDigits(1, 10));
+
+
+// Stworz funkcje, ktora bedzie zwracac informacje, czy w danym stringu znajduje sie dany ciag znakow
+
+
+const isInString = (string, phrase) => {
+  return string.includes(phrase);
+}
+
+const includesEr = isInString('Harry Potter', 'er');
+
+console.log(includesEr)
+
+const searchElement = (string, phrase) => {
+  if(string.includes(phrase)) {
+    return string;
+  }
+}
+
+const foundElement = searchElement('Harry Potte', 'er');
+
+
+// Uzywanie kolekcji (array obiektow) w funkcji
+
+const books2 = [
+  {
+    name: "Harry Potter",
+    category: "adventure"
+  },
+  {
+    name: 'Alice in Wonderland',
+    category: 'fantasy'
+  },
+  {
+    name: 'Lord of the Rings',
+    category: 'fantasy'
+  }
+]
+
+// PURE FUNCTION
+const filterElements = (collection, phrase) => {
+  return collection.filter(element => {
+    return element.name.includes(phrase)
+  })
+}
+
+console.log(filterElements(books2, 'er'));
+
+
+// 1. Stworz funkcje multiply, ktorej zadaniem bedzie wymnozyc przez siebie 2 liczby
+
+// multiply(3, 3) -> 9
+
+const multiply = (a,b) => a * b;
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+console.log(multiply(3,3))
+
+
+// 2. Stworz funkcje, ktora zwroci liczbe znakow z danego stringa
+
+const checkLetter = string => string.length;
+
+// function checkLetter(string) {
+//   return string.length;
+// }
+
+// checkLetter('Damian') -> 6
+
+// 3. Stworz funkcje findByCategory, ktora przyjmuje kolekcje i kategorie, a nastepnie wyszukaj wszystkie ksiazki, ktore maja podana kategorie.
+
+// findByCategory(books2, 'adventure') -> [{ob1}]
+
+const books3 = [
+  {
+    name: "Harry Potter",
+    category: "adventure"
+  },
+  {
+    name: 'Alice in Wonderland',
+    category: 'fantasy'
+  },
+  {
+    name: 'Lord of the Rings',
+    category: 'fantasy'
+  }
+]
+
+const findByCategory = (collection, category) => {
+  return collection.filter(item => {
+    return item.category === category
+  });
+}
+
+console.log(findByCategory(books3, 'fantasy'));
+
+// 4. Rozszerz zadanie 3 o warunek, ze jesli nie znajdziemy zadnej ksiazki o podanej kategorii, to funkcja wyswietli napis "No books"
+
+const findByCategoryExtended = (collection, category) => {
+  const filteredCollection = collection.filter(item => {
+    return item.category === category
+  });
+
+  if(filteredCollection.length === 0) {
+    return 'No Books'
+  }
+
+  return filteredCollection
+}
+
+console.log(findByCategoryExtended(books3, 'fantasy'));
+console.log(findByCategoryExtended(books3, 'fantas'));
+
+
+// ! ------- POZOSTALE FUNKCJE w PROG FUNKCYJNYM ----------
+
+// find
+
+const books4 = [
+  {
+    name: "Harry Potter",
+    category: "adventure"
+  },
+  {
+    name: 'Alice in Wonderland',
+    category: 'fantasy'
+  },
+  {
+    name: 'Lord of the Rings',
+    category: 'fantasy'
+  }
+]
+
+// Find sluzy do zlapania pierwszego elementu znalezionego
+
+const fantasyBook = books4.find(book => book.category === 'adventure')
+
+console.log(fantasyBook)
+
+// reduce
+
+const shopping3 = [
+  {
+    name: "Banan",
+    price: 3.99
+  },
+  {
+    name: "Wiśnie",
+    price: 19.99
+  },
+  {
+    name: "Chleb",
+    price: 4.99
+  },
+]
+
+const price3 = shopping3.reduce((accumulator, product) => {
+  console.log(accumulator);
+
+  return accumulator - product.price;
+}, 500)
+
+console.log(price3)
+
+// Sort
+
+const sortedShoppingList = shopping3.sort((a, b) => {
+  return a.price > b.price ? 1 : -1;
+})
+
+console.log(sortedShoppingList)
+console.log('-------------------------')
+// 1. Za pomocą funkcji sort, zwróc produkt, który jest najdrozszy
+const shopping4 = [
+  {
+    name: "Banan",
+    price: 3.99
+  },
+  {
+    name: "Wiśnie",
+    price: 19.99
+  },
+  {
+    name: "Chleb",
+    price: 4.99
+  },
+]
+
+const sortedShoppingList4 = shopping4.sort((a, b) => {
+  return a.price > b.price ? 1 : -1;
+})
+
+
+
+console.log(sortedShoppingList4);
+
+// 2. Za pomoca funkcji find, sprawdz cene chleba
+// 3. Za pomoca funkcji reduce, sprawdz srednia wzrostu w klasie. W klasie sa 3 osoby [178, 164, 199]
